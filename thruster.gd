218e7943fb0a415fc34539289ec759627d0888e4
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-08-29 11:00:42 -0700

    made modifications

--- thruster.gd
+++ thruster.gd
@@ -1,6 +1,35 @@
 extends "res://sfx/thruster.gd"
 
-func setup():
-	.setup()
-	audioLoop.pitch_scale = 1
-	audioStart.pitch_scale = 1
+onready var HeavyLoop = get_node_or_null("HeavyLoop")
+onready var HeavyStart = get_node_or_null("HeavyStart")
+var can_use = true
+
+func _ready():
+	if HeavyLoop == null:
+		can_use = false
+	if HeavyStart == null:
+		can_use = false
+
+func _physics_process(delta):
+	audioLoop.volume_db = -80
+	audioStart.volume_db = -80
+	if not can_use:
+		return
+	if not ship.setup or ship.frozen or ship.dummy:
+		return
+	if targetPower == 0and gimbalCurrent == gimbalTarget:
+		if HeavyLoop.is_playing():
+			HeavyLoop.stop()
+			HeavyStart.stop()
+		return
+	if targetPower>0 and playerControlled:
+		if not HeavyLoop.playing:
+			HeavyLoop.play()
+			HeavyStart.play()
+
+	else:
+		if HeavyLoop.is_playing():
+			HeavyLoop.stop()
+			HeavyStart.stop()
+	
+
